da12440f2a89a188b120193537ff276fe6545d36
Camélia AMIN HANDOYO <camhdyo@gmail.com>
2026-05-28 09:33:10 +1100

    ajout projet Vue.js taux de change

---
+++ tauxchange-aminandoyo-camelia/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  width: 100%;
+  min-height: 100vh;
+  overflow-x: hidden;
+}
+
+#app {
+  min-height: 100vh;
+}
+</style>
